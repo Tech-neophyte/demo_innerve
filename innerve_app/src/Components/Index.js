d433e0b3a1d043c3db75d827a379f6f01b806897
Tech-neophyte <aananditaa001btit22@igdtuw.ac.in>
2023-08-02 04:32:53 +0530

    First Commit

---
+++ innerve_app/src/Components/Index.js
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./testimonials";
+import Hero from "./Hero";
+
+export {
+  Navbar,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
---
+++ innerve_app/src/constants/index.js
@@ -0,0 +1,197 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "Events",
+    title: "Events",
+  },
+  {
+    id: "speakers",
+    title: "Speakers",
+  },
+  {
+    id: "About_us",
+    title: "About Us",
+  },
+];
+
+export const Events= [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Prizes",
+    content:
+      "Win exciting tech goodies and exclusive rewards.",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "Credibility",
+    content:
+      "An event with a proven track record of excellence.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Networking",
+    content:
+      "Connect with industry leaders and expand your professional circle.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Safety-first approach. Well-organized and secure.Winning prizes was a cherry on top!",
+    name: "Jensen",
+    title: "Attendee",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "A tech extravaganza! An event worth attending!",
+    name: "Steve",
+    title: "Attendee",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "Networking at its finest! Made valuable connections.",
+    name: "Kenn",
+    title: "Attendee",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Registrations",
+    value: "3800+",
+  },
+  {
+    id: "stats-2",
+    title: "Events",
+    value: "30+",
+  },
+  {
+    id: "stats-3",
+    title: "Collaberations",
+    value: "$20+",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.merriam-webster.com/dictionary/content",
+      },
+      {
+        name: "How it Works",
+        link: "https://www.youtube.com/watch?v=SGP6Y0Pnhe4",
+      },
+      {
+        name: "Create",
+        link: "https://www.merriam-webster.com/dictionary/create",
+      },
+      {
+        name: "Explore",
+        link: "https://www.merriam-webster.com/dictionary/explore",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.hoobank.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.youtube.com/watch?v=IJNR2EpS0jw",
+      },
+      {
+        name: "Partners",
+        link: "https://www.merriam-webster.com/dictionary/partners",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.merriam-webster.com/dictionary/suggestions",
+      },
+      {
+        name: "Blog",
+        link: "https://minervaforum.in/Layouts/mwrites.html",
+      },
+      {
+        name: "Newsletters",
+        link: "https://minervaforum.in/Layouts/content.html",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://taarangana.in/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://cloud.google.com/partners/become-a-partner",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/innerveigdtuw/?hl=en",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://twitter.com/Innerveigdtuw?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://in.linkedin.com/company/innerve",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
